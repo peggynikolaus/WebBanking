9710da39769cbe385f5d2bd2204bb1c0fc32bee5
peggy.nikolaus <peggy.nikolaus@telekom.de>
2022-02-14 14:05:24 +0100

    1.Commit

---
+++ src/main/java/de/telekom/sea7/WebBankingApplication.java
@@ -0,0 +1,14 @@
+package de.telekom.sea7;
+
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class WebBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBankingApplication.class, args);
+		
+
+	}
+
+}
